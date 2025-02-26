1d7f4654723ad8b97742e0004cef9429b8976047
shivinabox <35644450+shivinabox@users.noreply.github.com>
2025-02-26 23:37:06 +0530

    Create experience.js

---
+++ pages/experience.js
@@ -0,0 +1,8 @@
+export function Experience() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold">Experience</h1>
+      <p className="text-lg mt-2 text-gray-600">Summer Intern at Union Bank of India, developed a credit assessment framework for high-value corporate loans.</p>
+    </div>
+  );
+}
