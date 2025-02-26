268f692af019248ee2dc179837cb6eab844e40f7
shivinabox <35644450+shivinabox@users.noreply.github.com>
2025-02-26 23:34:37 +0530

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold">About Me</h1>
+      <p className="text-lg mt-2 text-gray-600">Investment Analyst with a strong background in finance and engineering.</p>
+    </div>
+  );
+}
